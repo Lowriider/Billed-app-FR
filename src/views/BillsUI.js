7b56657b4c6897032d75b4e3716a6e7fdc1c953d
Clément <lowriider23@gmail.com>
2021-12-09 13:48:51 +0100

    :bug: Fix date order

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -5,7 +5,7 @@ import LoadingPage from "./LoadingPage.js"
 import Actions from './Actions.js'
 
 const row = (bill) => {
-  return (`
+  return `
     <tr>
       <td>${bill.type}</td>
       <td>${bill.name}</td>
@@ -16,16 +16,21 @@ const row = (bill) => {
         ${Actions(bill.fileUrl)}
       </td>
     </tr>
-    `)
+    `;
   }
 
 const rows = (data) => {
+  if (data) {
+    const dataArray = data.sort((a, b) => 
+      new Date(b.date) - new Date(a.date)
+    );
+  }
   return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
 }
 
 export default ({ data: bills, loading, error }) => {
   
-  const modal = () => (`
+  const modal = () => `
     <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
@@ -40,7 +45,7 @@ export default ({ data: bills, loading, error }) => {
         </div>
       </div>
     </div>
-  `)
+  `;
 
   if (loading) {
     return LoadingPage()
